e4a7790e807d7f298da067b71766fc8183aa4c7d
Arthur-Woodlee <adamwoodlee136@icloud.com>
2025-10-09 13:23:50 +1100

    Fix Stylelint config with SCSS support

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,11 +1,12 @@
-// stylelint.config.js
+// stylelint.config.mjs
 export default {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-standard-scss'],
+  plugins: ['stylelint-scss'],
+  customSyntax: 'postcss-scss',
   rules: {
-    indentation: 2,
-    'string-quotes': 'single',
-    'color-hex-case': 'lower',
-    'block-no-empty': true,
+    'scss/at-rule-no-unknown': true,
+    'scss/dollar-variable-pattern': '^[_a-z][a-z0-9-]*$',
+    'scss/comment-no-empty': true,
     'selector-class-pattern': [
       '^[a-z][a-z0-9\\-]+$',
       {
